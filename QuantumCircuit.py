23ce5e1041a5963c42974d1fe8ef850884ee6548
Pontus Wikståhl <vikstal@chalmers.se>
2020-03-10 14:13:26 +0100

    Added some comments and minor changes to the code.

--- QuantumCircuit.py
+++ QuantumCircuit.py
@@ -6,11 +6,10 @@ def H_drive_coeff_x(t, args):
 
     # Amplitude, needs to be optimized to get perfect pi pulse or pi/2 pulse
     B = args['amp']
-
     # DRAG-parameter, needs to be optimized to get no phase errors in a pi/2 pulse
     q = args['qscale']
     drive_freq = args['freq']
-    L = args['L']
+    L = args['L'] # gate time
     sigma = args['sigma']
 
     # E(t)
@@ -27,7 +26,7 @@ def H_drive_coeff_y(t, args):
     # DRAG-parameter, needs to be optimized to get no phase errors in a pi/2 pulse
     q = args['qscale']
     drive_freq = args['freq']
-    L = args['L']
+    L = args['L'] # gate time
     sigma = args['sigma']
 
     # E(t)
@@ -36,8 +35,6 @@ def H_drive_coeff_y(t, args):
     I = E
     Q = -q * (t-0.5*L) / sigma * E # dE/dt
     return Q*np.cos(drive_freq * t) + I*np.sin(drive_freq * t)
-
-
 
 class QuantumCircuit:
 
@@ -60,9 +57,7 @@ class QuantumCircuit:
         self.num_q = len(qubits) # number of qubits
         self.cpl_map = cpl_map # coupling map
         self.cpl = cpl # couplings
-
-        # Rotating frame
-        rotating_freq = 0#2*np.pi * 5.0
+        self.rotating_freq = 2*np.pi * 5.0 # # rotating frame frequency
 
         # Construct the Qubit Hamiltonian
         H_qubits = 0
@@ -94,31 +89,30 @@ class QuantumCircuit:
         theta: is the angle
         """
 
-        # hamiltonian in rotating frame of drive
-        # drive at qubit resonance freq
+        # Hamiltonian in the rotating frame
         drive_freq = self.qubits[n].resonance_freq
-        print(drive_freq)
-        rotating_freq = drive_freq
-        a = qt.destroy(3)
-        H_qubit = (self.qubits[n].resonance_freq - rotating_freq)*a.dag()*a + (self.qubits[n].anharmonicity/2) * a.dag()**2 * a**2
+        rotating_freq = self.rotating_freq
+
+        # Annihilation operator for the n:th qubit
+        a = qt.destroy(n)
 
         # Drive Hamiltonian (since we have drag pulses, we need both x and y drive)
-        #
         H_drive_x = a.dag() + a
         H_drive_y = 1j*(a.dag() - a)
-        # Gaussian width in μs
-        sigma = 0.010 # = 10 ns
 
-        # Gate time in μs
-        L = 0.050 # = 50 ns
+        # Gaussian width in ns
+        sigma = 10
+
+        # Gate time in ns
+        L = 50
 
         # total Hamiltonian
         H = [H_qubit,
              [H_drive_x,H_drive_coeff_x],
              [H_drive_y,H_drive_coeff_y]
-        ]
+            ]
 
-        # Total time in μs
+        # Total time in ns
         t_total = L
         tlist = np.linspace(0,t_total,500)
 
@@ -132,8 +126,11 @@ class QuantumCircuit:
         B = theta/(2*np.abs(np.trapz(drive_x + 1j*drive_y, x=tlist)))
         print('Amplitude =',B)
 
+        # Amplitude
+        amp = B * (theta % np.pi) / np.pi
+
         # Master equation simulation
-        args = {'amp': B, 'qscale': 0.032, 'freq': (rotating_freq - drive_freq), 'L': L, 'sigma': sigma}
+        args = {'amp': amp, 'qscale': 0.032, 'freq': (rotating_freq - drive_freq), 'L': L, 'sigma': sigma}
         result = qt.mesolve(H, self.qubits[n].state, tlist, [], [], args=args)
         final_state = result.states[-1] # final state
 
